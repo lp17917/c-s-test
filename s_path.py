985170720ffbe5c1cc40737532525e8f9430a792
lp17917 <lp17917@bristol.ac.uk>
2021-08-17 18:44:07 +0100

    Added reading a line and separating each element

--- s_path.py
+++ s_path.py
@@ -3,7 +3,14 @@ import time
 
 def main():
     t0 = time.time()
-
+    f = open("tester.dat", "r")
+    a = []
+    x = f.readline()
+    a.append(x)
+    print(a[0])
+    f.close()
+    numbers = list(map(str, a[0].split()))
+    print(numbers)
     t1 = time.time()
     total = t1 - t0
     print(total)
