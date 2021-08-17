a377fa99967c25f0b15c37c257ebe8abe4c0e62f
lp17917 <lp17917@bristol.ac.uk>
2021-08-17 19:19:41 +0100

    Finished reading and formatting file

--- s_path.py
+++ s_path.py
@@ -3,14 +3,18 @@ import time
 
 def main():
     t0 = time.time()
-    f = open("tester.dat", "r")
-    a = []
-    x = f.readline()
-    a.append(x)
-    print(a[0])
+    f = open("exmouth-links.dat", "r")
+    start = []
+    finish = []
+    weight = []
+    for x in f:
+        a = list(map(str, x.split()))
+        start.append(a[0])
+        finish.append(a[1])
+        weight.append(int(a[2]))
     f.close()
-    numbers = list(map(str, a[0].split()))
-    print(numbers)
+    for count in range(len(start)):
+        print("Start: ", start[count], " Finish: ", finish[count], " Weight: ", weight[count])
     t1 = time.time()
     total = t1 - t0
     print(total)
