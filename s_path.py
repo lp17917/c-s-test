98521ca610f10e872e47f2e40ecef8b398ac656f
lp17917 <lp17917@bristol.ac.uk>
2021-08-21 17:45:58 +0100

    Finished reading all nodes and added unvisited as default

--- s_path.py
+++ s_path.py
@@ -1,23 +1,54 @@
+import sys
 import time
+
+
+def file_read(file):
+    f = open(file, "r")
+    s = []
+    e = []
+    w = []
+    for x in f:
+        a = list(map(str, x.split()))
+        s.append(a[0])
+        e.append(a[1])
+        w.append(int(a[2]))
+    f.close()
+    return s, e, w
+
+
+def name_exists(name, r_names):
+    for i in range(len(r_names)):
+        if name == r_names[i][0]:
+            return True
+    return False
+
+
+def read_name(name, r_names):
+    if not name_exists(name, r_names):
+        r_names.append([name, 0])
+        return True
+    return False
 
 
 def main():
     t0 = time.time()
-    f = open("exmouth-links.dat", "r")
-    start = []
-    finish = []
-    weight = []
-    for x in f:
-        a = list(map(str, x.split()))
-        start.append(a[0])
-        finish.append(a[1])
-        weight.append(int(a[2]))
-    f.close()
-    for count in range(len(start)):
-        print("Start: ", start[count], " Finish: ", finish[count], " Weight: ", weight[count])
+    filename = sys.argv[1]
+    origin = sys.argv[2]
+    destination = sys.argv[3]
+    print("File:", filename, "Origin:", origin, "Destination:", destination)
+    start, finish, weight = file_read(filename)
+    #for count in range(len(start)):
+        #print("Start: ", start[count], " Finish: ", finish[count], " Weight: ", weight[count])
     t1 = time.time()
     total = t1 - t0
     print(total)
+    names = []
+    for i in range(len(start)):
+        read_name(start[i], names)
+    for i in range(len(finish)):
+        read_name(finish[i], names)
+
+    print(names)
 
 
 if __name__ == "__main__":
