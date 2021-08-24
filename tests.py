e48c242a12e88458af16fbccf353771a04d90991
lp17917 <lp17917@bristol.ac.uk>
2021-08-24 12:18:28 +0100

    Added some priority queue tests

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
+import queue
 import unittest
+
+import dijkstra
 import s_path
 
 
@@ -43,10 +46,91 @@ class TestMethodsReading(unittest.TestCase):
                        ["J1041", "J1040", 1229, 105]]
 
         # Tests that each value in the table is the expected value for the exmouth-links file
-        for i in range(len(test_values)):
-            self.assertEqual(edges[test_values[i][3]][0], test_values[i][0])
-            self.assertEqual(edges[test_values[i][3]][1], test_values[i][1])
-            self.assertEqual(edges[test_values[i][3]][2], test_values[i][2])
+        for value in test_values:
+            self.assertEqual(edges[value[3]][0], value[0])
+            self.assertEqual(edges[value[3]][1], value[1])
+            self.assertEqual(edges[value[3]][2], value[2])
+
+    def test_node_exists(self):
+        nodes = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        self.assertTrue(s_path.node_exists("A", nodes))
+        self.assertTrue(s_path.node_exists("C", nodes))
+        self.assertTrue(s_path.node_exists("E", nodes))
+        self.assertTrue(s_path.node_exists("G", nodes))
+        self.assertTrue(s_path.node_exists("B", nodes))
+        self.assertFalse(s_path.node_exists("T", nodes))
+        self.assertFalse(s_path.node_exists("B ", nodes))
+        self.assertFalse(s_path.node_exists(5, nodes))
+
+    def test_add_nodes(self):
+        expectedoutput = ["J0001", "J0101", "J0052", "X0010", "J0123", "J1010"]
+        nodes = []
+        s_path.add_node("J0001", nodes)
+        self.assertEqual(expectedoutput[:1], nodes)
+        s_path.add_node("J0101", nodes)
+        self.assertEqual(expectedoutput[:2], nodes)
+        s_path.add_node("J0001", nodes)
+        self.assertEqual(expectedoutput[:2], nodes)
+        s_path.add_node("J0052", nodes)
+        self.assertEqual(expectedoutput[:3], nodes)
+        s_path.add_node("J0052", nodes)
+        self.assertEqual(expectedoutput[:3], nodes)
+        s_path.add_node("X0010", nodes)
+        s_path.add_node("J0123", nodes)
+        s_path.add_node("J1010", nodes)
+        self.assertEqual(expectedoutput, nodes)
+
+
+class TestMethodsQueue(unittest.TestCase):
+
+    def test_push(self):
+        pqueue = dijkstra.PriorityQueue()
+        element = ["A", "A", 0]
+        pqueue.push(element)
+        expected = [["A", "A", 0]]
+        self.assertEqual(expected, pqueue.queue)
+        element = ["C", "B", 4]
+        pqueue.push(element)
+        expected = [["A", "A", 0], ["C", "B", 4]]
+        self.assertEqual(expected, pqueue.queue)
+        element = ["J0001", "J0123", 9]
+        pqueue.push(element)
+        expected = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0123", 9]]
+        self.assertEqual(expected, pqueue.queue)
+        element = ["", "X10", 0]
+        pqueue.push(element)
+        expected = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0123", 9], ["", "X10", 0]]
+        self.assertEqual(expected, pqueue.queue)
+        element = [""]
+        pqueue.push(element)
+        expected = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0123", 9], ["", "X10", 0], [""]]
+        self.assertEqual(expected, pqueue.queue)
+
+    def test_pop(self):
+        pqueue = dijkstra.PriorityQueue()
+        addingeles = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0123", 9], ["", "X10", 1], ["B", "C", 5]]
+        for ele in addingeles:
+            pqueue.push(ele)
+        expected = sorted(addingeles, key=lambda x: x[2])
+        for ele in expected:
+            self.assertEqual(ele, dijkstra.PriorityQueue.pop(pqueue))
+
+    def test_update(self):
+        pqueue = dijkstra.PriorityQueue()
+        addingeles = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0123", 9], ["", "X10", 1], ["B", "C", 5]]
+        for ele in addingeles:
+            pqueue.push(ele)
+        pqueue.update_priority(0, ["A", "B", 5])
+        self.assertEqual(addingeles, pqueue.queue)
+        expected = [["A", "A", 0], ["C", "B", 4], ["J0001", "J0100", 3], ["", "X10", 1], ["B", "C", 5]]
+        pqueue.update_priority(2, ["J0001", "J0100", 3])
+        self.assertEqual(expected, pqueue.queue)
+
+    def test_isempty(self):
+        pass
+
+    def test_find(self):
+        pass
 
 
 if __name__ == '__main__':
